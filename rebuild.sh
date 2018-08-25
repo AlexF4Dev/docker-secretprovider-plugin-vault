2b7047f81e2461dd8773b5c628f01acbc93e6457
Sune Keller <absukl@almbrand.dk>
2018-08-25 11:03:13 +0200

    Add constraints - remove if you're on a single node Swarm
    Signed-off-by: Sune Keller <absukl@almbrand.dk>

--- rebuild.sh
+++ rebuild.sh
@@ -13,10 +13,10 @@ docker plugin create ${REGISTRY}/absukl/secrets-plugin:latest ${PWD}/plugin
 docker container run --detach --name vault --publish 8200:8200 vault server -dev -dev-root-token-id=1234
 docker plugin enable ${REGISTRY}/absukl/secrets-plugin:latest
 echo -n '1234' | docker secret create secret-zero
-docker service create --name vault-helper-service --secret secret-zero --restart-condition on-failure busybox tail -f /dev/null
+docker service create --constraint 'node.role == manager' --name vault-helper-service --secret secret-zero --restart-condition on-failure busybox tail -f /dev/null
 docker secret create --driver ${REGISTRY}/absukl/secrets-plugin haha
 docker secret create --driver ${REGISTRY}/absukl/secrets-plugin --label "dk.almbrand.docker.plugin.secretprovider.vault.type"="vault_token" VAULT_TOKEN_SORT_OF
-docker service create --detach --name snitch --secret haha --secret VAULT_TOKEN_SORT_OF busybox sh -c 'find /run/secrets -type f | xargs -i -n 1 sh -c "echo {}:; cat {}; echo"; sleep 2'
+docker service create --constraint 'node.role == worker' --detach --name snitch --secret haha --secret VAULT_TOKEN_SORT_OF busybox sh -c 'find /run/secrets -type f | xargs -i -n 1 sh -c "echo {}:; cat {}; echo"; sleep 2'
 docker container exec --env VAULT_ADDR=http://127.0.0.1:8200 --env VAULT_TOKEN=1234 vault vault kv put secret/haha value=sosecret
 docker service logs -f snitch
 exit $?
