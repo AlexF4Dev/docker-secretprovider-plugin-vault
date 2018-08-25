56227b430f6f3f193d81d8b373c18a77d1031faf
Sune Keller <absukl@almbrand.dk>
2018-08-25 11:17:58 +0200

    Only enable plugin after creating service
    Signed-off-by: Sune Keller <absukl@almbrand.dk>

--- rebuild.sh
+++ rebuild.sh
@@ -13,9 +13,9 @@ sudo docker export "$id" | sudo tar -x -C plugin/rootfs/
 docker rm -vf "$id"
 docker plugin create ${REGISTRY}/absukl/secrets-plugin:latest ${PWD}/plugin
 docker container run --detach --name vault --publish 8200:8200 vault server -dev -dev-root-token-id=1234
-docker plugin enable ${REGISTRY}/absukl/secrets-plugin:latest
 echo -n '1234' | docker secret create secret-zero -
 docker service create --constraint 'node.role == manager' --name vault-helper-service --secret secret-zero --restart-condition on-failure busybox tail -f /dev/null
+docker plugin enable ${REGISTRY}/absukl/secrets-plugin:latest
 docker secret create --driver ${REGISTRY}/absukl/secrets-plugin haha
 docker secret create --driver ${REGISTRY}/absukl/secrets-plugin --label "dk.almbrand.docker.plugin.secretprovider.vault.type"="vault_token" VAULT_TOKEN_SORT_OF
 docker node ls --filter role=worker -q | wc -l | grep -q 0 && snitch_role=manager || snitch_role=worker
